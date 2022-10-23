4d653ab17fb36ca1430119e7f3a33ca03e4cf646
ArtyomProgrammist <115404868+ArtyomProgrammist@users.noreply.github.com>
2022-10-24 01:27:02 +0300

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,7 +1,9 @@
 from os import *
 from sys import *
 from time import *
+from colorama import Fore as f
 system("clear")
+system(f"figlet {f.GREEN}PingHubOfficial")
 ping = input("Введите ссылку, чтобы проверить ping: ")
 system(f"ping {ping}")
 system(f"ping {ping}")
