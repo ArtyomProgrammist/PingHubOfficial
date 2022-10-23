8d08015ecd87a6a07a41dbd672972bfea6252938
ArtyomProgrammist <115404868+ArtyomProgrammist@users.noreply.github.com>
2022-10-24 01:19:36 +0300

    Create ping.py

---
+++ ping.py
@@ -0,0 +1,11 @@
+from os import *
+from sys import *
+from time import *
+system("clear")
+ping = input("Введите ссылку, чтобы проверить ping: ")
+system(f"ping {ping}")
+system(f"ping {ping}")
+exit(-2)
+while True:
+    sleep(1)
+    system("clear")
