dbea9863cbc314f90c42902f8d906b902ffa9eae
ArtyomProgrammist <115404868+ArtyomProgrammist@users.noreply.github.com>
2022-10-24 01:30:20 +0300

    Update ping.py

--- ping.py
+++ ping.py
@@ -8,7 +8,7 @@ system(f"figlet {f.GREEN}PingHubOfficial")
 ping = input("Введите ссылку, чтобы проверить ping: ")
 system(f"ping {ping}")
 system(f"ping {ping}")
-exit(-2)
 while True:
     sleep(1)
     system("clear")
+exit(-2)
